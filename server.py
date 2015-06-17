9626791786fdc4f22b1761719a8b0c48c8aa40bb
Denis Ivanets <denself@gmail.com>
2015-06-17 15:32:42 +0300

    Validate url parameters for requested image size

--- server.py
+++ server.py
@@ -5,9 +5,10 @@ from django.conf import settings
 
 __author__ = 'Denis Ivanets (denself@gmail.com)'
 
+TEST_SECRET_KEY = '(j3t0hye(j4tcvl&-0q7isq7f4wrz4_h=hfq4wdll77j^ztv!o'
 settings.configure(
     DEBUG=os.environ.get('DEBUG', 'on') == 'on',
-    SECRET_KEY=os.environ.get('SECRET_KEY', '(j3t0hye(j4tcvl&-0q7isq7f4wrz4_h=hfq4wdll77j^ztv!o'),
+    SECRET_KEY=os.environ.get('SECRET_KEY', TEST_SECRET_KEY),
     ROOT_URLCONF=__name__,
     ALLOWED_HOSTS=os.environ.get('ALLOWED_HOSTS', 'localhost').split(','),
     MIDDLEWARE_CLASSES=(
@@ -18,9 +19,27 @@ settings.configure(
 )
 
 
+from django import forms
 from django.conf.urls import url
 from django.core.wsgi import get_wsgi_application
 from django.http import HttpResponse
+
+
+class ImageForm(forms.Form):
+    """This form validates requested image"""
+
+    height = forms.IntegerField(min_value=1, max_value=2000)
+    width = forms.IntegerField(min_value=1, max_value=2000)
+
+
+def placeholder(request, width, height):
+    form = ImageForm({'width': width, 'height': height})
+    if form.is_valid():
+        height = form.cleaned_data['height']
+        width = form.cleaned_data['width']
+        return HttpResponse('Ok')
+    else:
+        return HttpResponse('Invalid Image requested')
 
 
 def index(request):
@@ -28,6 +47,7 @@ def index(request):
 
 
 urlpatterns = (
+    url(r'^image/(?P<width>\d+)x(?P<height>\d+)/$', placeholder),
     url(r'^$', index),
 )
 
